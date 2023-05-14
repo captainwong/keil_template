b9daa8242fb7ab9f8de448876b69d5825a581f2b
i7-8700 <1281261856@qq.com>
2023-05-14 16:33:13 +0800

    add uvproj

--- template/CleanSave.bat
+++ template/CleanSave.bat
@@ -1,7 +1,7 @@
 copy .\obj\template.hex .
 rd /s/q obj
 for /f %%i in ('.\tools\dt.exe') do set name="template_%%i.zip"
-zip -r %name% .\src .\tools .\CleanSave.bat .\template.hex
+zip -r %name% .\uvproj .\src .\tools .\CleanSave.bat .\template.hex
 del .\template.hex
 echo.
 pause

442c9acf79cba25d477203ce6858ec81625475ef
i7-8700 <1281261856@qq.com>
2023-05-14 16:28:18 +0800

    init

---
+++ keil_template/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// TODO: 自动化生成新工程
+// 工程名 new-project-name 由 argv[1] 输入
+// 自动创建文件夹 new-project-name
+// 自动改名 uvproj/*.uv* --> uvproj/new-project-name.uv*
+// 解析工程文件修改 name of executable: template --> new-project-name
+
+
+int main() {
+
+}
